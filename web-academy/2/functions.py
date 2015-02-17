e0ab1b91863fcb4200d22933afc35fe048009cb0
Vargentum <vargentum@gmail.com>
2015-02-17 17:58:21 +0200

    decision for lesson2 HW. All except 4-th exercise.

--- web-academy/2/functions.py
+++ web-academy/2/functions.py
@@ -4,20 +4,34 @@ def reverse(s):
     """
     Дзеркально обертає рядок
     """
-    pass
+    characters = list(s)
+    charactersLengh = len(s)
+    res = []
+    i = 1
+    while i <= charactersLengh:
+        res.append(characters[i * -1])
+        i += 1
+    return ''.join(res)
 
 assert reverse('abc') == 'cba'
+
 
 # 2
 def sum_of_squares_of_digits(n):
     """
     Рахує суму цифр числа
     """
-    pass
+    numbers = str(n)
+    res = 0
+    for number in numbers:
+        res += int(number)
+    return res
+
 
 assert sum_of_squares_of_digits(256) == 13
 assert sum_of_squares_of_digits(512) == 8
 assert sum_of_squares_of_digits(111) == 3
+
 
 # 3
 s = [1, 2, 3]
@@ -26,10 +40,17 @@ def reverse_in_place(c):
     Дзеркально повертає елементи списку, модифікуючи
     сам список
     """
-    pass
+    i = 1
+    while i <= len(c):
+        inverted = c.pop(i * -1)
+        c.append(inverted)
+        i += 1
+    return c
+
 
 reverse_in_place(s)
 assert s == [3, 2, 1]
+
 
 # *4
 def generate_substrings(s):
@@ -38,5 +59,6 @@ def generate_substrings(s):
     """
     pass
 
-assert generate_substrings('abc') == \
-    set('a', 'b', 'c', 'ab', 'bc', 'ac', 'abc')
+# assert generate_substrings('abc') == \
+    # set('a', 'b', 'c', 'ab', 'bc', 'ac', 'abc')
+
